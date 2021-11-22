939b3c5f58a02c0badb9eb5e37f791e7aed2ddfa
moriah Cesaretti <moriah@moriahs-new-mac.local>
2021-11-22 15:05:40 -0800

    Updating the requirements to include bs4 and prefect

--- prefect_pipeline/noaa_ndbc/process_historical_data.py
+++ prefect_pipeline/noaa_ndbc/process_historical_data.py
@@ -5,14 +5,23 @@ import os
 import pandas as pd
 from datetime import datetime
 from collections import defaultdict
-from typing import Dict, List, Any, TypedDict, Tuple, Set
+from typing import Dict, List, Any, TypedDict, Tuple, Set, Callable
 from bs4 import BeautifulSoup
 
 from compression.pigz import PigzReader, PigzWriter
-from prefect_pipeline.noaa_ndbc.models import NDBCStation, StandardMeteorologicalDataConversionPost2006, StandardMeteorologicalDataConversionPre2006
+from prefect_pipeline.noaa_ndbc.models import (
+    NDBCStation,
+    StandardMeteorologicalDataConversionPost2006,
+    StandardMeteorologicalDataConversionPre2006,
+)
 
+# TODO: convert this file into a class!
 
-def extract_filename_info(filename: str, logger, separator='h') -> Tuple[str, str]:
+def extract_filename_info(
+    filename: str,
+    logger: Any,
+    separator='h'
+) -> Tuple[str, str]:
     suffix = '.txt.gz'
     raw_name = filename.replace(suffix, '')
     # has to be 4 digits
@@ -22,17 +31,6 @@ def extract_filename_info(filename: str, logger, separator='h') -> Tuple[str, st
         station_id = raw_name[:-len(separator)]
         return station_id, year
     raise ValueError(f'File name format is invalid {filename}, {separator}')
-    # offset = -4-separator
-    # assert raw_name[offset:-4] == separator
-    # if historical:
-    #     # NOAA adds an h in between the station id and the year for historical data
-    #     assert separator == 'h', f'Thats weird {separator}, {filename}'
-    # else:
-    #     # NOAA adds the month int in between the station id and the year for historical data
-    #     # TODO-URGENT once the recent 
-    #     assert int(separator) in set(range(1, 10)), f'Thats weird {separator}, {filename}'
-
-    # return raw_name[:-5], year
 
 def build_station_id_list(filepath: str) -> Dict[str, NDBCStation]:
     # Load the stations into a dictionary lookup of stations id to NDBCStation
@@ -43,19 +41,46 @@ def build_station_id_list(filepath: str) -> Dict[str, NDBCStation]:
             station_lookup[station['id']] = station
     return station_lookup
 
-def get_year_specific_parsing(year):
+def get_year_specific_parsing(
+    year,
+) -> Tuple[List[List[str]],Callable[[str],datetime],Dict[str,str]]:
     year_number = int(year)
     if year_number < 1999:
-        return [['YY', "MM", 'DD', 'hh']], lambda x: datetime.strptime(x, "%y %m %d %H"), StandardMeteorologicalDataConversionPre2006
+        return (
+            [['YY', "MM", 'DD', 'hh']],
+            lambda x: datetime.strptime(x, "%y %m %d %H"),
+            StandardMeteorologicalDataConversionPre2006,
+        )
     if year_number < 2005:
-        return [['YYYY', "MM", 'DD', 'hh']], lambda x: datetime.strptime(x, "%Y %m %d %H"), StandardMeteorologicalDataConversionPre2006
+        return (
+            [['YYYY', "MM", 'DD', 'hh']],
+            lambda x: datetime.strptime(x, "%Y %m %d %H"),
+            StandardMeteorologicalDataConversionPre2006,
+        )
     if year_number < 2007:
-        return [['YYYY', "MM", 'DD', 'hh', 'mm']], lambda x: datetime.strptime(x, "%Y %m %d %H %M"), StandardMeteorologicalDataConversionPre2006
+        return (
+            [['YYYY', "MM", 'DD', 'hh', 'mm']],
+            lambda x: datetime.strptime(x, "%Y %m %d %H %M"),
+            StandardMeteorologicalDataConversionPre2006,
+        )
     if year_number >= 2007:
-        return [['#YY', "MM", 'DD', 'hh', 'mm']], lambda x: datetime.strptime(x, "%Y %m %d %H %M"), StandardMeteorologicalDataConversionPost2006
-    print('oops')
+        return (
+            [['#YY', "MM", 'DD', 'hh', 'mm']],
+            lambda x: datetime.strptime(x, "%Y %m %d %H %M"),
+            StandardMeteorologicalDataConversionPost2006,
+        )
 
-def process_file(station, i, year, station_lookup, ignore_first_row, null_values, year_to_process_as, total_stations, logger):
+def process_file(
+    station: Tuple[str, str],
+    i: int,
+    year: str,
+    station_lookup: Dict[str, NDBCStation],
+    ignore_first_row: Dict[str, bool],
+    null_values: Set,
+    year_to_process_as: str,
+    total_stations: int,
+    logger: Any,
+) -> pd.DataFrame:
     name, filepath = station
     parse_dates, date_parser, Conversion = get_year_specific_parsing(year_to_process_as)
     dimensions = station_lookup[name]
@@ -80,8 +105,15 @@ def process_file(station, i, year, station_lookup, ignore_first_row, null_values
         # this will allow for a more dynamic table and more diverse amount
         # of field value
         # We also need to parse out null values
-
-        date_rename = {'#YY_MM_DD_hh_mm': 'datetime_utc', 'YY_MM_DD_hh_mm': 'datetime_utc', 'YYYY_MM_DD_hh_mm': 'datetime_utc', '#YY_MM_DD_hh': 'datetime_utc', 'YY_MM_DD_hh': 'datetime_utc', 'YYYY_MM_DD_hh': 'datetime_utc'}
+        date_columns = set([
+            '#YY_MM_DD_hh_mm',
+            'YY_MM_DD_hh_mm',
+            'YYYY_MM_DD_hh_mm',
+            '#YY_MM_DD_hh',
+            'YY_MM_DD_hh',
+            'YYYY_MM_DD_hh',
+        ])
+        date_rename = {date_name: 'datetime_utc' for date_name in date_columns}
         data_df = data_df.rename(columns=Conversion)
         data_df = data_df.rename(columns=date_rename)
         data_df = data_df.melt(
@@ -102,14 +134,32 @@ def process_file(station, i, year, station_lookup, ignore_first_row, null_values
         if next_year > 2009:
             raise ValueError(v_err)
         print(f'Issue with file {name}, {year}: trying as {next_year} format')
-        return process_file(station, i, year, station_lookup, ignore_first_row,  null_values, str(next_year), total_stations)
+        return process_file(
+            station,
+            i,
+            year,
+            station_lookup,
+            ignore_first_row,
+            null_values,
+            str(next_year),
+            total_stations,
+        )
     except TypeError as t_err:
         print(t_err)
         next_year = int(year_to_process_as) + 1
         if next_year > 2009:
             raise TypeError(t_err)
         print(f'Issue with file {name}, {year}: trying as {next_year} format')
-        return process_file(station, i, year, station_lookup, ignore_first_row,  null_values, str(next_year), total_stations)
+        return process_file(
+            station,
+            i,
+            year,
+            station_lookup,
+            ignore_first_row,
+            null_values,
+            str(next_year),
+            total_stations,
+        )
     except pd.errors.EmptyDataError:
         logger.warning(f'Empty file found for {station}')
         return pd.DataFrame([])
@@ -118,8 +168,8 @@ def process_and_write_files(
     stations_by_year: Dict[str, Tuple[str, str]],
     station_lookup: Dict[str, NDBCStation],
     output_directory: str,
-    logger,
-):
+    logger: Any,
+) -> Dict[str,str]:
     ignore_first_row = { str(i): i >= 2007 for i in range(1970, 2022)}
     null_values = set([99, 99.0, 99.00, 999, 999.0, 999.00, 9999.0,])
     # print(f'Starting row processing: {stations_by_year.keys()}')
@@ -128,21 +178,39 @@ def process_and_write_files(
         total_stations = len(stations_by_year[year])
         logger.info(f'Processing year {year}, {total_stations}')
         # print(f'Processing year {year}, {total_stations}')
-        output_file_path = os.path.join(output_directory, f'processed_rows.{year}.json.gz')
+        output_file_path = os.path.join(
+            output_directory,
+            f'processed_rows.{year}.json.gz',
+        )
         output_files[year] = output_file_path
         with PigzWriter(output_file_path) as output_file:
             for i, station in enumerate(stations_by_year[year]):
-                data_df = process_file(station, i, year, station_lookup, ignore_first_row, null_values, year, total_stations, logger)
-                # logger.info(f'DATA DF: {data_df.head()}')
+                data_df = process_file(
+                    station,
+                    i,
+                    year,
+                    station_lookup,
+                    ignore_first_row,
+                    null_values,
+                    year,
+                    total_stations,
+                    logger,
+                )
                 data_df.to_json(output_file, orient='records', date_format='iso')
     return output_files
 
 
-def group_files(files: Dict[str,Dict[str,str]], station_lookup: Dict[str, NDBCStation], logger) -> Dict[str, Set[Tuple[str, str]]]:
+def group_files(
+    files: Dict[str,Dict[str,str]],
+    station_lookup: Dict[str, NDBCStation],
+    logger: Any,
+) -> Dict[str, Set[Tuple[str, str]]]:
     stations_by_year = defaultdict(set)
     for file_path, file_info in files.items():
         # Extract the station id and year from the title
-        station_id, year, month = file_info['station_id'], file_info['year'], file_info['month']
+        station_id = file_info['station_id']
+        year = file_info['year']
+        month = file_info['month']
         if station_id in station_lookup:
             # Filter out stations we have no station metadata for.
             # A half data point isn't useful. without a location its just non-sense
